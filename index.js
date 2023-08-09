ec50d7b901ddbf09bef08545df2a26a4e76939b5
Muntasir <munmamun9@gmail.com>
2023-08-09 20:50:10 +0800

    new

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const num1Input = document.getElementById('num1');
-const num2Input = document.getElementById('num2');
-const operationSelect = document.getElementById('operation');
-const calculateBtn = document.getElementById('calculateBtn');
-const resultParagraph = document.getElementById('result');
+const num1P = document.getElementById('num1')
 
-calculateBtn.addEventListener('click', () => {
-    const num1 = parseFloat(num1Input.value);
-    const num2 = parseFloat(num2Input.value);
-    const operation = operationSelect.value;
-    let result;
+const num2P = document.getElementById('num2')
 
-    if (operation === 'add') {
-        result = num1 + num2;
-    } else if (operation === 'subtract') {
-        result = num1 - num2;
-    } else if (operation === 'multiply') {
-        result = num1 * num2;
-    } else if (operation === 'divide') {
-        result = num1 / num2;
+const operationP = document.getElementById('operation')
+
+const calculateBtnP = document.getElementById('calculateBtn')
+
+const resultP = document.getElementById('result')
+
+calculateBtnP.addEventListener('click', function(){
+    const num1 = parseFloat(num1P.value)
+    const num2 = parseFloat(num2P.value)
+    const operation = operationP.value
+
+    let result
+
+    if (operation === 'add'){
+
+    result = num1 + num2}
+
+    else if (operation === 'subtract') {
+        result = num1 - num2
     }
 
-    resultParagraph.textContent = `Result: ${result}`;
-});
+    else if (operation === 'multiply'){
+        result = num1 * num2
+    }
+
+    else if (operation === 'divide') {
+        result = num1 / num2
+    }
+
+    resultP.textContent = `Result ${result}`
+
+})
